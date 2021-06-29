ae5692eca77ce6f6f78acb3e5fe156da5caf5f9d
victor vargas <vict.vargass@duocuc.cl>
2021-06-28 23:11:22 -0400

    url inicio agregada

--- desp_proyecto/core_app/urls.py
+++ desp_proyecto/core_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import home, inicio_real
 urlpatterns = [
     path('', home, name="home"),
+    path('inicio', inicio_real, name="inicio")
     ]
