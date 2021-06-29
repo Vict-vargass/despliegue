265e4f83d3a5b02c9046ee32cfb95ff920ac7f67
victor vargas <vict.vargass@duocuc.cl>
2021-06-28 22:54:40 -0400

    view de inicio creada

--- desp_proyecto/core_app/views.py
+++ desp_proyecto/core_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'core_app/index.html')
+
+def inicio_real(request):
+    return render(request, 'core_app/home.html')
